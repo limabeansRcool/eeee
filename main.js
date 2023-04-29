a01f8386cd97cfeebbe3701a2ff00ba3fbc8904b
limabeansRcool <102268583+limabeansRcool@users.noreply.github.com>
2023-04-28 19:46:44 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+<html>
+       
+<head>
+
+<title>thing</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> </link>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/p5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.9.0/addons/p5.dom.min.js"></script>
+
+<script src="https://unpkg.com/ml5@latest/dist/ml5.min.js" type="text/javascript"></script>
+
+
+  
+<link rel="stylesheet" type="text/css" href="style.css"></link>
+</head>
+
+<body> 
+
+  <div class="btn btn-warning">Case study- Google lense VS mobilenet 
+  
+  </div>
+  <ol>
+      <div class="test_image_and_result col-xs-12"> 
+      <li class="col-sm-4 col-xs-12">
+          <b>Test image- </b>
+          <img src="book.png" class="img-responsive" > </img>
+                  </li>
+                  <h5 class="col-sm-4 col-xs-12">Output on <b>GOOGLE LENS</b>       <span> </span> </h5>
+                  <h5 class="col-sm-4 col-xs-12">Output on <b>MOBILENET</b>       <span> </span> </h5>
+     
+
+      </div>
+  </ol>
+
+</body>
+
+</html>
